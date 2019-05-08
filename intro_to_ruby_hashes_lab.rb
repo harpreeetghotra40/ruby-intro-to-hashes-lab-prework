2b5d6fd9228f7b5750910b6333fc6b0bd7e34646
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-08 23:33:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,17 +39,17 @@ def monopoly_with_third_tier
 	monopoly = {
 		railroads: {
 			:pieces => 4,
-			:names => {
-				:reading_railroad => {}
-				:pennsylvania_railroad => {}
-				:b_and_o_railroad => {}
-				:shortline => {}
-			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
 				:four_pieces_owned => 200
+			},
+			:names => {
+				:reading_railroad => {}
+				:pennsylvania_railroad => {}
+				:b_and_o_railroad => {}
+				:shortline => {}
 			}
 		}
 	}
@@ -60,6 +60,12 @@ def monopoly_with_fourth_tier
 	monopoly = {
 		railroads: {
 			:pieces => 4,
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
+			},
 			:names => {
 				:reading_railroad => {
 					:mortgage_value => "$100"
@@ -73,13 +79,7 @@ def monopoly_with_fourth_tier
 				:shortline => {
 					:mortgage_value => "$800"
 				}
-			},
-			:rent_in_dollars => {
-				:one_piece_owned => 25,
-				:two_pieces_owned => 50,
-				:three_pieces_owned => 100,
-				:four_pieces_owned => 200
-			},
+			}
 		}
 	}
 end
